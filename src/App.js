a0e687ed6eb9171bdf646bc9f45e65e1beb2dda3
Kelly Schumacher <kschumacher93@gmail.com>
2024-03-05 12:24:29 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Route, Routes, useLocation } from "react-router-dom";
+// import axios from 'axios';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/categories" element={<CategoriesPage />} />
+      <Route path="/categories/:category" element={<SelectCategoryPage />} />
+      <Route path="/mostloved" element={<MostLovedPage />} />
+      <Route path="/mosttalkedabout" element={<MostCommentedPage />} />
+      <Route path="/somethingnew" element={<SomethingNewPage />} />
+      <Route path="/mostsaved" element={<MostSavedPage />} />
+    </Routes>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <nav>
+    //       <ul>
+    //         <li>Home</li>
+    //         <li>About</li>
+    //         <li>Projects</li>
+    //         <li>Contact</li>
+    //       </ul>
+    //     </nav>
+    //     <span className="hero-img"></span>
+    //     <h1>My App</h1>
+    //   </header>
+    //   <main></main>
+    //   <footer></footer>
+    // </div>
   );
 }
 
