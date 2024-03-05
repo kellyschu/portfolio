21456b6aeea39b9f8ba93a85759eb963855e3c3f
Kelly Schumacher <kschumacher93@gmail.com>
2024-03-05 16:38:42 -0500

    styling for the nav bar

--- src/App.js
+++ src/App.js
@@ -1,36 +1,96 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import { Route, Routes, useLocation } from "react-router-dom";
-// import axios from 'axios';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from "react-router-dom";
+import HomePage from "./Pages/HomePage/HomePage";
+import AboutPage from "./Pages/AboutPage/AboutPage";
+import ProjectsPage from "./Pages/ProjectsPage/ProjectsPage";
+import ContactPage from "./Pages/ContactPage/ContactPage";
+import "./App.scss";
+import Logo from "./assests/icons/KSD.V4.png";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/search" element={<SearchPage />} />
-      <Route path="/categories" element={<CategoriesPage />} />
-      <Route path="/categories/:category" element={<SelectCategoryPage />} />
-      <Route path="/mostloved" element={<MostLovedPage />} />
-      <Route path="/mosttalkedabout" element={<MostCommentedPage />} />
-      <Route path="/somethingnew" element={<SomethingNewPage />} />
-      <Route path="/mostsaved" element={<MostSavedPage />} />
-    </Routes>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <nav>
-    //       <ul>
-    //         <li>Home</li>
-    //         <li>About</li>
-    //         <li>Projects</li>
-    //         <li>Contact</li>
-    //       </ul>
-    //     </nav>
-    //     <span className="hero-img"></span>
-    //     <h1>My App</h1>
-    //   </header>
-    //   <main></main>
-    //   <footer></footer>
-    // </div>
+    <Router>
+      <div>
+        <nav>
+          <ul
+            className="hero-img"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "calc(15px - 1rem)",
+              justifyContent: "flex-end",
+              backgroundColor: "whitesmoke",
+              height: "80px",
+              margin: "0",
+              padding: " 20px",
+              width: "100%",
+            }}
+          >
+            <li
+              style={{
+                marginRight: "auto",
+              }}
+            >
+              <h3>kelly schumacher</h3>
+              <p>software developer</p>
+            </li>
+            <li
+              style={{
+                listStyleType: "none",
+                textDecoration: "none",
+              }}
+            >
+              <NavLink
+                to="/"
+                activeClassName="active"
+                style={{ color: "#fff" }}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li style={{ listStyleType: "none" }}>
+              <NavLink
+                to="/about"
+                activeClassName="active"
+                style={{ color: "#fff" }}
+              >
+                About
+              </NavLink>
+            </li>
+            <li style={{ listStyleType: "none" }}>
+              <NavLink
+                to="/projects"
+                activeClassName="active"
+                style={{ color: "#fff" }}
+              >
+                Projects
+              </NavLink>
+            </li>
+            <li style={{ listStyleType: "none" }}>
+              <NavLink
+                to="/contact"
+                activeClassName="active"
+                style={{ color: "#fff" }}
+              >
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <p>home</p>
+        </li>
+      </ul>
+    </div>
+  );
+}
